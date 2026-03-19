04ee68b63757cc024e3a4027e16cab1951fa9852
Harry Jung <harryjung135@gmail.com>
2026-03-18 20:14:35 -0600

    modified cycling behaviour

--- run_everest.cpp
+++ run_everest.cpp
@@ -7,7 +7,7 @@
 #include <direct.h>
 
 // test config //
-bool cycleSensors = 1;
+bool cycleSensors = 0;
 bool hasGPS = 1;
 bool hasIMU1 = 1;
 bool hasIMU2 = 1;
@@ -17,7 +17,8 @@ float deltaTime = 0.333f;  // note this is still from taberLaunch. maybe we can
 float stationaryTime = 0.0f;
 ///////////////////////////
 
-int sensorThisCycle = 1;
+int sensorThisCycle = 0;
+
 float everest_time = 0.0f;
 float timestamp = 0.0f;
 
@@ -37,12 +38,28 @@ float findClosestTime(float timestamp) {
 }
 
 int getSensorData(EverestTask* everest, int i, int stationary) {
-  float pressure;
   IMUData_Everest sensorData;
   IMUData_Everest sensorData2;
   BarosData baro1;
   BarosData baro2;
   float gps;
+
+  float accelX = taberLaunch[i][1];
+  float accelY = taberLaunch[i][2];
+  float accelZ = taberLaunch[i][3];
+
+  // Parse gyroscope readings (X, Y, Z)
+  float gyroX = taberLaunch[i][4];
+  float gyroY = taberLaunch[i][5];
+  float gyroZ = taberLaunch[i][6];
+
+  // Parse magnetometer readings (X, Y, Z)
+  float magX = taberLaunch[i][7];
+  float magY = taberLaunch[i][8];
+  float magZ = taberLaunch[i][9];
+
+  // Parse pressure readings
+  float pressure = baroData[i][1];
 
   if (stationary) {
     pressure = baroData[0][1];
@@ -60,26 +77,11 @@ int getSensorData(EverestTask* everest, int i, int stationary) {
     baro2 = {everest_time, pressure, 0, 0};
 
     gps = findClosestTime((float)everest_time);
+
   } else {
     // Tokenize the line using strtok
     // Parse accelerometer readings (X, Y, Z)
     timestamp = taberLaunch[i][0];
-    float accelX = taberLaunch[i][1];
-    float accelY = taberLaunch[i][2];
-    float accelZ = taberLaunch[i][3];
-
-    // Parse gyroscope readings (X, Y, Z)
-    float gyroX = taberLaunch[i][4];
-    float gyroY = taberLaunch[i][5];
-    float gyroZ = taberLaunch[i][6];
-
-    // Parse magnetometer readings (X, Y, Z)
-    float magX = taberLaunch[i][7];
-    float magY = taberLaunch[i][8];
-    float magZ = taberLaunch[i][9];
-
-    // Parse pressure readings
-    float pressure = baroData[i][1];
 
     sensorData = {
         timestamp, gyroX,  gyroY, gyroZ, accelX,
@@ -99,6 +101,7 @@ int getSensorData(EverestTask* everest, int i, int stationary) {
   }
 
   // get measurements (this will be done from subscribing to the sensors)
+  // simulates 5 sensor readings being made per 1/3rd sec.
   if (cycleSensors) {
     switch (sensorThisCycle) {
       case 0:
@@ -106,15 +109,34 @@ int getSensorData(EverestTask* everest, int i, int stationary) {
         sensorThisCycle++;
         break;
       case 1:
+        sensorData2 = {
+            timestamp + (1.0f / 15.0f),  // (1/3) / 5
+            gyroX,
+            gyroY,
+            gyroZ,
+            accelX,
+            accelY,
+            accelZ,
+            magX,
+            magY,
+            magZ,
+        };
         everest->IMU2_Measurements(sensorData2);
         sensorThisCycle++;
         break;
       case 2:
+        baro1 = {timestamp + (2.0f / 15.0f), pressure, 0, 0};
         everest->Baro1_Measurements(baro1);
         sensorThisCycle++;
         break;
       case 3:
+        baro2 = {timestamp + (3.0f / 15.0f), pressure, 0, 0};
         everest->Baro2_Measurements(baro2);
+        sensorThisCycle++;
+        break;
+      case 4:
+        gps = findClosestTime(float(timestamp + (4.0f / 15.0f)));
+        everest->GPS_Measurements(gps);
         sensorThisCycle = 0;
         break;
     }
@@ -166,13 +188,22 @@ int main() {
   }
 
   for (int i = 0; i < taberLaunch.size(); i++) {
-    getSensorData(&everest, i, 0);
-
     // start timer for iteration
     start = std::clock();
 
-    // calls the entirety of the power of HALO, peak modularization
-    std::vector<float> haloData = everest.QueueEverest(timestamp);
+    if (cycleSensors) {  // If not using a time window, we poll and call everest
+                         // as fast as possible. here we will poll a sensor then
+                         // run 5 times every 1/3rd sec.
+      for (int j = 0; j < 5; j++) {
+        getSensorData(&everest, i, 0);
+        std::vector<float> haloData =
+            everest.QueueEverest(timestamp + ((float)j / 15.0f));
+      }
+    } else {  // if using a time window, we will collect 5 measurements in 1/3rd
+              // sec then run the filter.
+      getSensorData(&everest, i, 0);
+      std::vector<float> haloData = everest.QueueEverest(timestamp);
+    }
 
     clock_t endTime = std::clock();
 
